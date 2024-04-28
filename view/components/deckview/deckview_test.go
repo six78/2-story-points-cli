5e4d75314c019ac46ee2d55e5f3eaf0c601dabe7
Igor Sirotin <igor.sirotin.1012@gmail.com>
2024-04-28 19:16:00 +0100

    More deckview tests. Change CI job name.

--- view/components/deckview/deckview_test.go
+++ view/components/deckview/deckview_test.go
@@ -3,6 +3,7 @@ package deckview
 import (
 	"fmt"
 	"github.com/brianvoe/gofakeit/v6"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
 	"testing"
@@ -166,6 +167,7 @@ func (s *Suite) TestIncrementCursor() {
 }
 
 func (s *Suite) TestUpdate() {
+	issueID := protocol.IssueID(gofakeit.Letter())
 	deck := make(protocol.Deck, 3)
 	gofakeit.Slice(deck)
 
@@ -175,14 +177,14 @@ func (s *Suite) TestUpdate() {
 	model = model.Update(messages.GameStateMessage{
 		State: nil,
 	})
-	
+
 	s.Require().Equal(protocol.Deck{}, model.deck)
 	s.Require().Equal(protocol.IdleState, model.voteState)
 
 	model = model.Update(messages.GameStateMessage{
 		State: &protocol.State{
 			Deck:          deck,
-			ActiveIssue:   "1",
+			ActiveIssue:   issueID,
 			VotesRevealed: false,
 		},
 	})
@@ -202,4 +204,129 @@ func (s *Suite) TestUpdate() {
 	model = model.Update(messages.CommandModeChange{CommandMode: false})
 	s.Require().False(model.commandMode)
 
+	value := protocol.VoteValue(gofakeit.Letter())
+	model = model.Update(messages.MyVote{Result: protocol.VoteResult{
+		Value: value,
+	}})
+	s.Require().Equal(value, model.myVote)
+
+}
+
+func (s *Suite) TestCursor() {
+	issueID := protocol.IssueID(gofakeit.Letter())
+	deck := make(protocol.Deck, 3)
+	gofakeit.Slice(deck)
+
+	model := New(true)
+	_ = model.Init()
+
+	s.Require().True(model.focused)
+	s.Require().False(model.commandMode)
+
+	// Switch to VotingState as dealer
+	model = model.Update(messages.RoomJoin{
+		IsDealer: true,
+	})
+	model = model.Update(messages.GameStateMessage{
+		State: &protocol.State{
+			Deck:          deck,
+			ActiveIssue:   issueID,
+			VotesRevealed: false,
+		},
+	})
+	s.Require().True(model.isDealer)
+	s.Require().Equal(protocol.VotingState, model.voteState)
+
+	// Ensure initial positions
+	s.Require().Equal(0, model.VoteCursor())
+	s.Require().Equal(0, model.FinishCursor())
+
+	// Setup helpers
+	keyRight := tea.KeyMsg{Type: tea.KeyRight}
+	keyLeft := tea.KeyMsg{Type: tea.KeyLeft}
+
+	inc := func(value int, condition bool) int {
+		if condition {
+			return value + 1
+		}
+		return value
+	}
+
+	testCursors := func(vote bool, finish bool) {
+		initialVote := model.VoteCursor()
+		initialFinish := model.FinishCursor()
+
+		// Require not to be at the end of the deck
+		s.Require().Less(initialVote, len(deck)-1)
+		s.Require().Less(initialFinish, len(deck)-1)
+
+		model = model.Update(keyRight)
+		s.Require().Equal(inc(initialVote, vote), model.VoteCursor())
+		s.Require().Equal(inc(initialFinish, finish), model.FinishCursor())
+
+		model = model.Update(keyLeft)
+		s.Require().Equal(initialVote, model.VoteCursor())
+		s.Require().Equal(initialFinish, model.FinishCursor())
+	}
+
+	// Check deck cursor movement, ensure finish cursor stays in place
+	testCursors(true, false)
+
+	// Switch to revealed state
+	model = model.Update(messages.GameStateMessage{
+		State: &protocol.State{
+			Deck:          deck,
+			ActiveIssue:   issueID,
+			VotesRevealed: true,
+			Issues: protocol.IssuesList{
+				&protocol.Issue{ID: issueID},
+			},
+		},
+	})
+	s.Require().Equal(protocol.RevealedState, model.voteState)
+
+	// Move finish cursor to the middle to check no movement in both directions
+	model = model.Update(keyRight)
+	s.Require().Equal(1, model.FinishCursor())
+
+	// Check finish cursor movement, ensure deck cursor stays in place
+	testCursors(false, true)
+
+	// Switch to non-dealer
+	model = model.Update(messages.RoomJoin{IsDealer: false})
+	s.Require().Equal(protocol.RevealedState, model.voteState)
+
+	// Check both cursors stay in place
+	testCursors(false, false)
+
+	// Disable focus
+	model.Blur()
+	testCursors(false, false)
+
+	// Enable focus, enable command mode
+	model.Focus()
+	model = model.Update(messages.CommandModeChange{CommandMode: true})
+	testCursors(false, false)
+
+	// Change vote state to not voting or revealed
+	model = model.Update(messages.GameStateMessage{
+		State: &protocol.State{
+			Deck: deck,
+		},
+	})
+	testCursors(false, false)
+}
+
+func (s *Suite) TestBorderStyle() {
+	style := cardBorderStyle(false, false)
+	s.Require().Equal(&defaultBorderStyle, style)
+
+	style = cardBorderStyle(true, false)
+	s.Require().Equal(&votedBorderStyle, style)
+
+	style = cardBorderStyle(false, true)
+	s.Require().Equal(&highlightBorderStyle, style)
+
+	style = cardBorderStyle(true, true)
+	s.Require().Equal(&highlightBorderStyle, style)
 }
