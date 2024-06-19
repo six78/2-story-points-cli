9a59e5a6bc56cb4021b3083f7ca9c981ed9fe394
Igor Sirotin <igor.sirotin.1012@gmail.com>
2024-06-19 23:46:56 +0100

    test: fleets

---
+++ internal/transport/fleet_test.go
@@ -0,0 +1,33 @@
+package transport
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestFleets(t *testing.T) {
+	enr, ok := FleetENRTree(WakuSandbox)
+	require.True(t, ok)
+	require.Equal(t, fleets[WakuSandbox], enr)
+
+	enr, ok = FleetENRTree(ShardsTest)
+	require.True(t, ok)
+	require.Equal(t, fleets[ShardsTest], enr)
+
+	enr, ok = FleetENRTree(WakuTest)
+	require.False(t, ok) // We know this fleet, but it's not supported
+}
+
+func TestFleetSharded(t *testing.T) {
+	require.True(t, ShardsTest.IsSharded())
+	require.True(t, ShardsStaging.IsSharded())
+	require.False(t, WakuSandbox.IsSharded())
+	require.False(t, WakuTest.IsSharded())
+	require.False(t, FleetName(gofakeit.LetterN(5)).IsSharded())
+}
+
+func TestFleetDefaultPubsubTopic(t *testing.T) {
+	require.Equal(t, "/waku/2/default-waku/proto", WakuSandbox.DefaultPubsubTopic())
+	require.Equal(t, "/waku/2/rs/16/64", ShardsTest.DefaultPubsubTopic())
+}
