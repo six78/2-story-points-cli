65baf6ae0cdbdc4564f7702d7ff729cb9ee147aa
Igor Sirotin <igor.sirotin.1012@gmail.com>
2024-07-04 11:18:38 +0100

    chore: more refactor

--- cmd/2sp/demo/demo.go
+++ cmd/2sp/demo/demo.go
@@ -21,6 +21,17 @@ import (
 	"github.com/six78/2-story-points-cli/pkg/protocol"
 )
 
+var links = []string{
+	"https://github.com/golang/go/issues/26492",
+	"https://github.com/golang/go/issues/27605",
+	"https://github.com/golang/go/issues/64997",
+}
+var votes = [][]string{
+	{"3", "5", "3"},
+	{"5", "8", "8"},
+	{"13", "13", "8"},
+}
+
 type Demo struct {
 	ctx     context.Context
 	dealer  *game.Game
@@ -32,11 +43,11 @@ type Demo struct {
 	playerSubs []game.StateSubscription
 }
 
-func New(ctx context.Context, game *game.Game, program *tea.Program) *Demo {
+func New(ctx context.Context, dealer *game.Game, program *tea.Program) *Demo {
 	return &Demo{
 		ctx:     ctx,
-		dealer:  game,
-		state:   game.SubscribeToStateChanges(),
+		dealer:  dealer,
+		state:   dealer.SubscribeToStateChanges(),
 		program: program,
 		logger:  config.Logger.Named("demo"),
 	}
@@ -50,35 +61,58 @@ func (d *Demo) Stop() {
 	}
 }
 
+func (d *Demo) initializePlayers() error {
+	names := []string{"Alice", "Bob", "Charlie"}
+	d.players = make([]*game.Game, len(names))
+	d.playerSubs = make([]game.StateSubscription, len(names))
+
+	wg := sync.WaitGroup{}
+	errChan := make(chan error, len(names))
+
+	for i, name := range names {
+		wg.Add(1)
+		go func(i int, name string) {
+			defer wg.Done()
+			player, err := d.createPlayer(name)
+			if err != nil {
+				errChan <- errors.Wrap(err, "failed to create player")
+				return
+			}
+			d.players[i] = player
+			d.playerSubs[i] = player.SubscribeToStateChanges()
+		}(i, name)
+	}
+
+	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (d *Demo) Routine() {
 	defer d.Stop()
 
 	d.logger.Info("started")
-
-	// TODO: wait for the program to start
-	time.Sleep(2 * time.Second)
-	d.logger.Info("initial wait finished")
+	time.Sleep(2 * time.Second) // Wait for the program to start
 
 	// Create new room
 	d.sendShortcut(commands.DefaultKeyMap.NewRoom)
 	d.logger.Info("room created")
 
 	// Add players
-	playersNames := []string{"Alice", "Bob", "Charlie"}
-	d.players = make([]*game.Game, 0, 3)
-	d.playerSubs = make([]game.StateSubscription, 0, 3)
-
-	for _, name := range playersNames {
-		player, err := d.createPlayer(name)
-		if err != nil {
-			d.logger.Error("failed to create player", zap.Error(err))
-			return
-		}
-		d.players = append(d.players, player)
-		d.playerSubs = append(d.playerSubs, player.SubscribeToStateChanges())
+	err := d.initializePlayers()
+	if err != nil {
+		d.logger.Error("failed to initialize players", zap.Error(err))
+		return
 	}
 
-	err := d.waitForPlayers(d.players)
+	err = d.waitForPlayers(d.players)
 	if err != nil {
 		d.logger.Error("failed to wait for players", zap.Error(err))
 		return
@@ -90,17 +124,6 @@ func (d *Demo) Routine() {
 	d.logger.Info("switched to issues view")
 
 	// Add issues
-	links := []string{
-		"https://github.com/golang/go/issues/26492",
-		"https://github.com/golang/go/issues/27605",
-		"https://github.com/golang/go/issues/64997",
-	}
-	votes := [][]string{
-		{"3", "5", "3"},
-		{"5", "8", "8"},
-		{"13", "13", "8"},
-	}
-
 	d.sendText(strings.Join(links, "\n"))
 	d.logger.Info("issues added")
 	time.Sleep(500 * time.Millisecond)
@@ -112,12 +135,12 @@ func (d *Demo) Routine() {
 		return
 	}
 
+	// Players vote for issues
 	issues := d.dealer.CurrentState().Issues
 	for i, issue := range issues {
 		err = d.issueSubRoutine(issue.ID, votes[i])
 		if err != nil {
-			d.logger.Error("failed to run issue sub-routine",
-				zap.Error(err),
+			d.logger.Error("failed to run issue sub-routine", zap.Error(err),
 				zap.String("issueLink", issue.TitleOrURL))
 			return
 		}
@@ -138,18 +161,21 @@ func (d *Demo) Routine() {
 }
 
 func (d *Demo) issueSubRoutine(issueID protocol.IssueID, votes []string) error {
-	errChan := make(chan error)
+	errChan := make(chan error, len(votes))
 	wg := sync.WaitGroup{}
 	wg.Add(len(votes))
 	for i, vote := range votes {
-		go d.playerVoteSubroutine(i, issueID, protocol.VoteValue(vote), &wg, errChan)
+		go func(index int, vote string) {
+			defer wg.Done()
+			errChan <- d.playerVoteSubroutine(index, issueID, protocol.VoteValue(vote))
+		}(i, vote)
 	}
 
 	// Deal first issue (expect all players to be subscribed to state changes
 	d.sendKey(tea.KeyEnter, 1*time.Second)
 	d.logger.Info("deal issue")
 
-	// TODO: dealer vote by arrows
+	// Dealer vote with UI
 	d.sendKey(tea.KeyRight, 500*time.Millisecond)
 	d.sendKey(tea.KeyRight, 500*time.Millisecond)
 	d.sendKey(tea.KeyEnter, 0)
@@ -157,7 +183,9 @@ func (d *Demo) issueSubRoutine(issueID protocol.IssueID, votes []string) error {
 	wg.Wait()
 	close(errChan)
 	for err := range errChan {
-		return err
+		if err != nil {
+			return err
+		}
 	}
 	d.logger.Info("players voted")
 
@@ -186,16 +214,13 @@ func (d *Demo) issueSubRoutine(issueID protocol.IssueID, votes []string) error {
 	return nil
 }
 
-func (d *Demo) playerVoteSubroutine(index int, issueID protocol.IssueID, vote protocol.VoteValue, wg *sync.WaitGroup, errChan chan error) {
-	defer wg.Done()
-
+func (d *Demo) playerVoteSubroutine(index int, issueID protocol.IssueID, vote protocol.VoteValue) error {
 	player := d.players[index]
 	playerName := player.Player().Name
 
 	err := d.waitForIssueDealt(d.playerSubs[index], issueID)
 	if err != nil {
-		errChan <- errors.Wrap(err, fmt.Sprintf("%s: issue not dealt", playerName))
-		return
+		return errors.Wrap(err, fmt.Sprintf("%s: issue not dealt", playerName))
 	}
 
 	// Random delay to simulate human behavior
@@ -205,9 +230,10 @@ func (d *Demo) playerVoteSubroutine(index int, issueID protocol.IssueID, vote pr
 	// Publish vote
 	err = player.PublishVote(vote)
 	if err != nil {
-		errChan <- errors.Wrap(err, fmt.Sprintf("%s: failed to publish vote", playerName))
-		return
+		return errors.Wrap(err, fmt.Sprintf("%s: failed to publish vote", playerName))
 	}
+
+	return nil
 }
 
 func (d *Demo) sendShortcut(key key.Binding) {
