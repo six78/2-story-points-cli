990854b06c29a4cc8d0f5d9a1ab35a5ca7d68963
Igor Sirotin <igor.sirotin.1012@gmail.com>
2024-07-09 23:53:16 +0100

    wip

--- cmd/2sp/main.go
+++ cmd/2sp/main.go
@@ -1,69 +1,77 @@
 package main
 
 import (
-	"context"
-	"os"
+	"fmt"
+	"runtime/debug"
 
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/jonboulle/clockwork"
-	"go.uber.org/zap"
-
-	"github.com/six78/2-story-points-cli/cmd/2sp/demo"
 	"github.com/six78/2-story-points-cli/internal/config"
-	"github.com/six78/2-story-points-cli/internal/transport"
-	"github.com/six78/2-story-points-cli/internal/view"
-	"github.com/six78/2-story-points-cli/pkg/game"
 	"github.com/six78/2-story-points-cli/pkg/storage"
 )
 
 func main() {
-	config.ParseArguments()
-	config.SetupLogger()
 
-	ctx, quit := context.WithCancel(context.Background())
-	defer quit()
+	info, ok := debug.ReadBuildInfo()
+	fmt.Println(info, ok)
 
-	waku := transport.NewNode(ctx, config.Logger)
-	defer waku.Stop()
+	//short := versioninfo.Short()
+	//fmt.Println(short)
 
-	options := []game.Option{
-		game.WithContext(ctx),
-		game.WithTransport(waku),
-		game.WithStorage(createStorage()),
-		game.WithLogger(config.Logger.Named("game")),
-		game.WithPlayerName(config.PlayerName()),
-		game.WithOnlineMessagePeriod(config.OnlineMessagePeriod),
-		game.WithStateMessagePeriod(config.StateMessagePeriod),
-		game.WithEnableSymmetricEncryption(config.EnableSymmetricEncryption),
-		game.WithClock(clockwork.NewRealClock()),
-	}
+	//fmt.Println("Version:", versioninfo.Version)
+	//fmt.Println("Revision:", versioninfo.Revision)
+	//fmt.Println("DirtyBuild:", versioninfo.DirtyBuild)
+	//fmt.Println("LastCommit:", versioninfo.LastCommit)
 
-	game := game.NewGame(options)
-	if game == nil {
-		config.Logger.Fatal("could not create game")
-	}
-	defer game.Stop()
+	//program.Println("ok: %w", ok)
 
-	// Create UI model and program
-	model := view.InitialModel(game, waku)
-	program := tea.NewProgram(model)
-
-	// Run demo if enabled
-	if config.Demo() {
-		demonstration := demo.New(ctx, game, program)
-		go func() {
-			demonstration.Routine()
-			program.Quit()
-		}()
-	}
-
-	if _, err := program.Run(); err != nil {
-		config.Logger.Error("error running program", zap.Error(err))
-		os.Exit(1)
-		return
-	}
-
-	os.Exit(0)
+	//
+	//config.ParseArguments()
+	//config.SetupLogger()
+	//
+	//ctx, quit := context.WithCancel(context.Background())
+	//defer quit()
+	//
+	//waku := transport.NewNode(ctx, config.Logger)
+	//defer waku.Stop()
+	//
+	//options := []game.Option{
+	//	game.WithContext(ctx),
+	//	game.WithTransport(waku),
+	//	game.WithStorage(createStorage()),
+	//	game.WithLogger(config.Logger.Named("game")),
+	//	game.WithPlayerName(config.PlayerName()),
+	//	game.WithOnlineMessagePeriod(config.OnlineMessagePeriod),
+	//	game.WithStateMessagePeriod(config.StateMessagePeriod),
+	//	game.WithEnableSymmetricEncryption(config.EnableSymmetricEncryption),
+	//	game.WithClock(clockwork.NewRealClock()),
+	//}
+	//
+	//game := game.NewGame(options)
+	//if game == nil {
+	//	config.Logger.Fatal("could not create game")
+	//}
+	//defer game.Stop()
+	//
+	//// Create UI model and program
+	//model := view.InitialModel(game, waku)
+	//program := tea.NewProgram(model)
+	//
+	//
+	//// Run demo if enabled
+	//if config.Demo() {
+	//	demonstration := demo.New(ctx, game, program)
+	//	go func() {
+	//		demonstration.Routine()
+	//		program.Quit()
+	//	}()
+	//}
+	//
+	//if _, err := program.Run(); err != nil {
+	//	config.Logger.Error("error running program", zap.Error(err))
+	//	os.Exit(1)
+	//	return
+	//}
+	//
+	//os.Exit(0)
 }
 
 func createStorage() storage.Service {
